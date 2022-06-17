f8bee6959e6b3d9eb5a323c04511449271f1420a
Wilson Neto <wn_br@hotmail.com>
2022-06-16 21:48:09 -0300

    wip/feat: string to binary tree

--- Helpers/Extensions/StringToBinaryTreeExtension.cs
+++ Helpers/Extensions/StringToBinaryTreeExtension.cs
@@ -13,15 +13,23 @@ public static class StringToBinaryTreeExtension
     {
         if(valueExtractor == null && typeof(T) == typeof(int))
             valueExtractor = stringValue => int.Parse(stringValue) as T;
-        var nodes = input.Replace("[","").Replace("]","").Split(',').Select(x => x.Trim());
+        var nodes = input.Replace("[","").Replace("]","").Split(',').Select(x => x.Trim()).ToList();
+        var totalItems = nodes.Count;
+        if(totalItems == 0)
+            throw new InvalidCastException("The sequence should have at leat one item");
+
         var queue = new Queue<T?>();
         foreach(var nodeValueInString in nodes)
         {
             var extractedNode = nodeValueInString == emptyEquivalent ? null : valueExtractor?.Invoke(nodeValueInString);
             queue.Enqueue(extractedNode);
         }
-        var root = new Node<T>(default!);
-        var queue = new Queue<Node<T>>();
+        var root = queue.Dequeue();
+        if(root is null)
+            throw new InvalidCastException("The tree root node shouldn't be null");
+
+        new Tree<T>(new Node<T>(root));
+        
         return null;
     }
 }
